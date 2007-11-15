fda834e2035ba6002000139e8ed260ca8d3edbd3
monsur <monsur@94bfccf8-353e-0410-8c7d-21009be9e2f0>
2007-11-15 04:42:29 +0000

    Merged from branch

---
+++ content/debug.js
@@ -0,0 +1,5 @@
+function LOG(msg) {
+  var consoleService = Components.classes["@mozilla.org/consoleservice;1"]
+                                 .getService(Components.interfaces.nsIConsoleService);
+  consoleService.logStringMessage(msg);
+}
