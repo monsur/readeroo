05d2f170ac536cd7496e6d1c3c9dc47ae3cff443
monsur <monsur@94bfccf8-353e-0410-8c7d-21009be9e2f0>
2008-04-12 20:47:17 +0000

    Pruning old branches

--- branches/Readeroo_0_3/content/debug.js
+++
@@ -1,5 +0,0 @@
-function LOG(msg) {
-    var consoleService = Components.classes["@mozilla.org/consoleservice;1"]
-                                   .getService(Components.interfaces.nsIConsoleService);
-    consoleService.logStringMessage(msg);
-}
