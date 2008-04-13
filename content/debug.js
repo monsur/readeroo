3b42a750734a41eb85243504e23b2aa60653c023
monsur <monsur@94bfccf8-353e-0410-8c7d-21009be9e2f0>
2008-04-13 21:54:16 +0000

    A bunch of refactoring in anticipation of some more refactoring.

--- content/debug.js
+++ content/debug.js
@@ -1,5 +1,5 @@
 function LOG(msg) {
     var consoleService = Components.classes["@mozilla.org/consoleservice;1"]
-                                   .getService(Components.interfaces.nsIConsoleService);
+        .getService(Components.interfaces.nsIConsoleService);
     consoleService.logStringMessage(msg);
 }
--- content/helpers.js
+++ content/helpers.js
@@ -1,41 +1,91 @@
-// handles xmlhttp request to server
+/**
+ * handles xmlhttp request to server
+ */
 var WebHelper = {
-    send: function(url,request,callback) {
-        var http = new XMLHttpRequest();
-        var mode = request?"POST":"GET";
-        http.open(mode,url,true);
-        if(mode=="POST"){
-            http.setRequestHeader('Content-Type','application/x-www-form-urlencoded');
+  
+  /**
+   * Sends an http GET request to a remote server
+   * @param {String} url The url to send the request to.
+   * @param {Object} args The querystring arguments
+   * @param {Function} callback
+   * @param {Function} errorCallback
+   */
+  send: function(url, args, callback, errorCallback) {
+    url = this.createUrl(url, args);
+    var http = new XMLHttpRequest();
+    http.open("GET",url,true);
+    http.onreadystatechange = function() {
+      if (http.readyState != 4) {
+        return;
+      }
+      if (http.status != 200) {
+        // Handle error
+        if (errorCallback) {
+          errorCallback(http);
         }
-        http.onreadystatechange=function(){if(http.readyState==4){callback(http.responseText);}};
-        http.send(request);
-    },
-
-    send2: function(url, args, callback) {
-        url = WebHelper.createUrl(url, args);
-        var http = new XMLHttpRequest();
-        http.open("GET",url,true);
-        http.onreadystatechange=function(){
-            if(http.readyState==4) {
-                callback(http.responseText);
-            }
-        };
-        http.send(null);
-    },
-
-    // squish together args array and url to create the full url
-    createUrl: function(url, args) {
-
-        // join the querystring args
-        var qs = '';
-        for (key in args) {
-            qs = qs + encodeURIComponent(key) + '=' + encodeURIComponent(args[key]) + '&';
-        }
-
-        // slap the querystring args onto the url
-        if (qs != '') {
-            url = url + '?' + qs;
-        }
-        return url;
+        return;
+      }
+      callback(http.responseText);
     }
+    http.send('');
+  },
+
+  /**
+   * Merges the inputs to create a url string with query parameters.
+   * @param {String} url The url
+   * @param {Object} args Set of key/value pairs for each querystring parameter.
+   */
+  createUrl: function(url, args) {
+
+    // join the querystring args
+    var qs = '';
+    for (key in args) {
+      qs = qs + encodeURIComponent(key) + '=' +
+          encodeURIComponent(args[key]) + '&';
+    }
+
+    // slap the querystring args onto the url
+    if (qs != '') {
+      url = url + '?' + qs;
+    }
+    return url;
+  }
+};
+
+/**
+ * Interacts with the browser window to retrieve and set the current url
+ */
+DocumentHelper = {
+
+  initialize: function() {
+    var wm = Components.classes['@mozilla.org/appshell/window-mediator;1']
+        .getService(Components.interfaces.nsIWindowMediator);
+    var recentWindow = wm.getMostRecentWindow('navigator:browser');
+    this.document = null;
+    if (recentWindow) {
+      this.document = recentWindow.content.document;
+    }
+  },
+
+  getTitle: function() {
+    if (this.document == null) {
+      return 'no title';
+    }
+    var titles = this.document.getElementsByTagName('title');
+    if ((!titles)  || (titles.length == 0)) {
+      return 'no title';
+    }
+    return titles[0].innerHTML;
+  },
+
+  getUrl: function() {
+    if (this.document == null) {
+      return '';
+    }
+    return this.document.location;
+  },
+
+  redirect: function(url) {
+    this.document.location.url = url;
+  }
 };
