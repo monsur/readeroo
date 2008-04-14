5533fc54324acfbf828f146d5cc8060f12d7b7fa
monsur <monsur@94bfccf8-353e-0410-8c7d-21009be9e2f0>
2008-04-14 02:26:20 +0000

    More whitespace magic

--- trunk/content/deliciousitem.js
+++ trunk/content/deliciousitem.js
@@ -10,14 +10,14 @@ function DeliciousItem() {
 }
 
 DeliciousItem.prototype.getArgs = function() {
-    var args = {};
-    args['url'] = this.url;
-    args['description'] = this.description;
-    args['extended'] = this.notes;
-    args['tags'] = this.tags.join(' ');
-    if (this.shared == false) {
-        args['shared'] = 'no';
-    }
-    return args;
+  var args = {};
+  args['url'] = this.url;
+  args['description'] = this.description;
+  args['extended'] = this.notes;
+  args['tags'] = this.tags.join(' ');
+  if (this.shared == false) {
+    args['shared'] = 'no';
+  }
+  return args;
 }
 
